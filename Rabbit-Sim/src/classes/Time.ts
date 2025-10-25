bf66c0130b21d68c017b9dc045379d90e9c06b60
Lewis Tough <2770287t@student.gla.ac.uk>
2025-10-25 13:09:47 +0100

    time class

---
+++ Rabbit-Sim/src/classes/Time.ts
@@ -0,0 +1,11 @@
+class Time {
+    day : boolean
+    night : boolean
+    dayNum : number 
+
+    constructor(day : boolean, night : boolean, dayNum : number) {
+    this.day = day
+    this.night = night
+    this.dayNum = dayNum
+    }
+}
