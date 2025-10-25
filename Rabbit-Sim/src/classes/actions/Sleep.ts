1582514c68281789897cec65f1ff765c6ff103ea
yao <zhenyaoheng@gmail.com>
2025-10-25 16:32:21 +0100

    chore: bug fixing sleep and upgradeMilitary action

--- Rabbit-Sim/src/classes/actions/Sleep.ts
+++ Rabbit-Sim/src/classes/actions/Sleep.ts
@@ -2,9 +2,8 @@ import type { Action } from "./Action";
 import { Colony } from "../Colony";
 
 export class Sleep implements Action {
-    takeAction(actor: Colony): number {
+    takeAction(actor: Colony, target?: Colony, context?: any): void | Promise<void> {
         actor.population *= 1.2;
         actor.energy *= 1.2;
-        return 1;
     }
 }
