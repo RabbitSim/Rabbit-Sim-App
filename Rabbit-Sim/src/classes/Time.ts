a843f1c0f546632061a6b430d196d7500aa97e29
Lewis Tough <2770287t@student.gla.ac.uk>
2025-10-25 13:42:13 +0100

    nextTur function

--- Rabbit-Sim/src/classes/Time.ts
+++ Rabbit-Sim/src/classes/Time.ts
@@ -4,10 +4,22 @@ class Time {
     dayNum : number 
     turnNum : number
 
-    constructor(day : boolean, night : boolean, dayNum : number, turnNum : number) {
-    this.day = day
-    this.night = night
-    this.dayNum = dayNum
-    this.turnNum = turnNum
+    constructor() {
+    this.day = true
+    this.night = false
+    this.dayNum = 0
+    this.turnNum = 0
     }
+    nextTurn(){
+        this.turnNum += 1;
+        if (this.turnNum == 4){
+            this.night = true;
+        }
+        if (this.turnNum == 9){
+            this.turnNum = 0;
+            this.dayNum++;
+            this.day = true;
+        }
+    }
+    
 }
