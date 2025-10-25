09e74bea97e4ac090b27ebce8d9f9b628aeceef9
Lewis Tough <2770287t@student.gla.ac.uk>
2025-10-25 16:33:45 +0100

    clean up

--- Rabbit-Sim/src/classes/actions/Sleep.ts
+++ Rabbit-Sim/src/classes/actions/Sleep.ts
@@ -2,7 +2,7 @@ import type { Action } from "./Action";
 import { Colony } from "../Colony";
 
 export class Sleep implements Action {
-    takeAction(actor: Colony, target?: Colony, context?: any): void | Promise<void> {
+    takeAction(actor: Colony, target?: Colony, context?: any): void {
         actor.population *= 1.2;
         actor.energy *= 1.2;
     }
