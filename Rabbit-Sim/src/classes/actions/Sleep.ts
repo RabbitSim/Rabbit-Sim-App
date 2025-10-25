fa94a7221253fc017b98babfc8a6e2f196f7ea89
yao <zhenyaoheng@gmail.com>
2025-10-25 20:23:25 +0100

    implemented sleep, upgrade defence, upgrade offence

--- Rabbit-Sim/src/classes/actions/Sleep.ts
+++ Rabbit-Sim/src/classes/actions/Sleep.ts
@@ -1,9 +1,11 @@
 import type { Action } from "./Action";
 import { Colony } from "../Colony";
+import { ColonyMath } from "../math/ColonyMath";
 
 export class Sleep implements Action {
     takeAction(actor: Colony, target?: Colony, context?: any): void {
-        actor.population *= 1.2;
-        actor.energy *= 1.2;
+        actor.energy = Math.min(actor.energy * 1.2, 100);
+        actor.population += ColonyMath.populationGrowth(actor.population, 0.04, actor.foodStorage * 2);
+        console.log(`${actor.name} is sleeping. Energy restored to ${actor.energy.toFixed(2)}. Population is now ${actor.population}.`);
     }
 }
