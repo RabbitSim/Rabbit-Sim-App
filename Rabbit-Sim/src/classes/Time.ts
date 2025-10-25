11d51203dd385e1a5376887baca51a8cb27cbea5
Lewis Tough <2770287t@student.gla.ac.uk>
2025-10-25 13:20:37 +0100

    turn num

--- Rabbit-Sim/src/classes/Time.ts
+++ Rabbit-Sim/src/classes/Time.ts
@@ -2,10 +2,12 @@ class Time {
     day : boolean
     night : boolean
     dayNum : number 
+    turnNum : number
 
-    constructor(day : boolean, night : boolean, dayNum : number) {
+    constructor(day : boolean, night : boolean, dayNum : number, turnNum : number) {
     this.day = day
     this.night = night
     this.dayNum = dayNum
+    this.turnNum = turnNum
     }
 }
